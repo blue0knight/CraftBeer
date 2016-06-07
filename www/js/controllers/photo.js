2fcd37b11b6d04d1589e567114669ee1be3979b7
blue0knight <oni_rivera98@yahoo.com>
2016-06-07 13:26:53 -0400

    added take photo and upload photo buttons

--- www/js/controllers/photo.js
+++ www/js/controllers/photo.js
@@ -2,4 +2,12 @@
 
 app.controller('PhotoCtrl', function($scope) {
 
+  $scope.takePhoto = function(){
+    console.log('take photoPhoto was clicked');
+  }
+
+  $scope.choosePhoto = function(){
+    console.log('choosePhoto was clicked');
+  }
+
 });
